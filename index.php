cc0273f085b5037f903ef91ff5bc8edda0dcf4dc
Tiffany White <tiffanywhite@dhcp-10-4-0-58.wireless.rochester.edu>
2016-10-11 13:39:26 -0400

    validation correction

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 <?php include "inc/footer.inc"; ?>
 
 
-	</div>
+	
 
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 		<script src="sss/sss.min.js"></script>
