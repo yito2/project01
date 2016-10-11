fd6591a6f69cdc386ed6ac6b689007f88198f68a
lukamadeleine <luka@Lukas-MacBook-Pro.local>
2016-10-10 22:26:56 -0400

    JS script

--- index.php
+++ index.php
@@ -53,5 +53,11 @@
 
 	</div>
 
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="sss/sss.min.js"></script>
+		<script>
+			$('.slider').sss();
+		</script>
+
 	</body>
 </html>
