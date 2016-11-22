0423e7173d0f57ee7adbb173d611e2430c2da79e
yito2 <yito2@u.rochester.edu>
2016-11-22 14:57:33 -0500

    More cosmetic changes

--- chinese-foods.php
+++ chinese-foods.php
@@ -25,61 +25,44 @@
 
 		<h3>Regional Cuisines</h3>
 		
-		<figure>
 			<img src="images/chinesefood.jpg" alt="chinese food">
-		</figure>
 		<p>
 		    A number of different styles contribute to Chinese cuisine but perhaps the best known and most influential are Cantonese cuisine, Shandong cuisine, Jiangsu cuisine (specifically Huaiyang cuisine) and Sichuan cuisine.[23][24][25] These styles are distinctive from one another due to factors such as availability of resources, climate, geography, history, cooking techniques and lifestyle.[26] One style may favour the use of lots of garlic and shallots over lots of chilli and spices, while another may favour preparing seafood over other meats and fowl.
-		</p>
-		<p>
+
 		    Jiangsu cuisine favours cooking techniques such as braising and stewing, while Sichuan cuisine employs baking, just to name a few.[23] Hairy crab is a highly sought after local delicacy in Shanghai, as it can be found in lakes within the region. Peking duck and dim-sum are other popular dishes well known outside of China.[23] 
-			
-		</p>
-		<p>
+
 			Cantonese cuisine，Dim sum, Cantonese term for small hearty dishes.[1] These bite-sized portions are prepared using traditional cooking methods such as frying, steaming, stewing and baking. It is designed so that one person may taste a variety of different dishes. Some of these may include rice rolls, lotus leaf rice, turnip cakes, buns, jiaozi-style dumplings, stir-fried green vegetables, congee porridge, soups, etc. The Cantonese style of dining, yum cha, combines the variety of dim sum dishes with the drinking of tea. Yum cha literally means 'drink tea'.[1]
-		</p>
-		<p>
+
 			Sichuan (spelled Szechuan in the once-common postal romanization), is a style of Chinese cuisine originating from the Sichuan Province of southwestern China famed for bold flavors, particularly the pungency and spiciness resulting from liberal use of garlic and chili peppers, as well as the unique flavor of the Sichuan peppercorn and Facing heaven pepper. Peanuts, sesame paste and ginger are also prominent ingredients in this style.
-		</p>
-		<p>
+
 			Based on the raw materials and ingredients used, the method of preparation and cultural differences, a variety of foods with different flavors and textures are prepared in different regions of the country. Many traditional regional cuisines rely on basic methods of preservation such as drying, salting, pickling and fermentation.[27]
 		</p>
 		
 	</article>
 
 
-        <aside class="column2">
+    <aside class="column2">
         	
 
 		<h3>Staple Foods</h3>
-		<figure>
 			<img src="images/rice.jpeg" alt="rice">
-		</figure>
 		<p>
 			Rice is a major staple food for people from rice farming areas in southern China.[citation needed] Steamed rice, usually white rice, is the most commonly eaten form. Rice is also used to produce beers, wines and vinegars. Rice is one of the most popular foods in China and is used in many dishes. Glutinous rice ("sticky rice") is a variety of rice used in many specialty Chinese dishes.
-		</p>
-		<figure>
+				
 			<img src="images/noodle.jpg" alt="noodle">
-			<figcaption>Chinese Dan Dan Noodle</figcaption>
-		</figure>
-		<p>
+
 			Chinese noodles come dry or fresh in a variety of sizes, shapes and textures and are often served in soups or fried as toppings. Some varieties, such as Shou Mian (literally noodles of longevity), are symbolic of long life and good health according to Chinese tradition.[23] Noodles can be served hot or cold with different toppings, with broth, and occasionally dry (as is the case with mi-fun). Noodles are commonly made with rice flour or wheat flour, but other flours such as soybean are also used.
-		</p>
-		<p>
+
 			Some common vegetables used in Chinese cuisine include Chinese leaves, bok choy (Chinese cabbage), dao-mieu (Chinese spinach), on choy, yu choy, bitter melon, and Chinese broccoli or gailan (guy-lahn). Other vegetables include bean sprouts, pea vine tips, watercress, celery.
-		</p>
-		<p>
+
 			Spices and seasonings such as fresh ginger root, garlic, scallion, white pepper, and sesame oil are widely used in many regional cuisines. Sichuan peppercorns, star anise, cinnamon, fennel, cilantro, parsley, and cloves are also used.[28][29]
 		</p>
 
-
-		
-		</aside>
+	</aside>
 
 
 
 		<section class="Citations">
-
 
              <h5>Citations</h5>
                 <ul>
